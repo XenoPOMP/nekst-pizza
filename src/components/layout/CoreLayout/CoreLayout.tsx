4498ce8fb5320d8297117bb9115677cca4168e0c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-04 20:01:09 +0300

    Initial commit

---
+++ src/components/layout/CoreLayout/CoreLayout.tsx
@@ -0,0 +1,38 @@
+import { type PropsWith } from '@xenopomp/advanced-types';
+import { type FC } from 'react';
+
+import { Footer, Header } from '@/src/components/layout';
+
+import type { CoreLayoutProps } from './CoreLayout.props';
+
+/**
+ * Wrapper for core layout. Provides access to fully manageable
+ * page layout.
+ *
+ * @param children
+ * @constructor
+ *
+ * @example
+ * // Header and footer will be added
+ * // automatically
+ * <CoreLayout>
+ *   <main>
+ *     Index page
+ *   </main>
+ * </CoreLayout>
+ */
+const CoreLayout: FC<PropsWith<'children', CoreLayoutProps>> = ({
+  children,
+}) => {
+  return (
+    <>
+      <Header />
+
+      {children}
+
+      <Footer />
+    </>
+  );
+};
+
+export default CoreLayout;
