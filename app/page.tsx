530cc102277aaa4f3e8e068d38f372e4a9a8ef69
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-04 21:22:16 +0300

    🔍 update seo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import cn from 'classnames';
 import { type Metadata } from 'next';
 
+import { AppConstants } from '@/app/app.constants.ts';
 import { CoreLayout } from '@/src/components/layout';
 import { UiContainer } from '@/src/components/ui';
 import { generateStaticMetadata } from '@/src/utils/seo';
@@ -8,7 +9,7 @@ import { generateStaticMetadata } from '@/src/utils/seo';
 import styles from './main-page.module.scss';
 
 export const metadata: Metadata = generateStaticMetadata({
-  title: 'Main',
+  title: `${AppConstants.appName} | Вкусно будет точно!`,
 });
 
 export default function Home() {
